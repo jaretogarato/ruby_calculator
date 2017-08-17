139eabc3caa422a4fbee18068c0d22d21f72faa2
Jared Gallardo <jaredgg@gmail.com>
2017-08-17 16:31:28 -0600

    cleaned up code; improved error checking

--- calculator-one-line-entry.rb
+++ calculator-one-line-entry.rb
@@ -45,91 +45,50 @@ def get_input
     oper_loc = input.index('/')
   end
 
-  left_hand = input[0..oper_loc - 1].split.join('')
-  left_hand2 = check_number(left_hand)
+  puts ">>>>>> #{oper_loc}"
+  if oper_loc
+    puts 'we have an operator location'
 
-  oper = input[oper_loc].split.join''
+    left_hand = input[0..oper_loc - 1].split.join('')
+    left_hand2 = check_number(left_hand)
 
-  right_hand = input[oper_loc + 1..input.length].split.join('')
-  right_hand2 = check_number(right_hand)
+    oper = input[oper_loc].split.join('')
 
-  puts '-------------------------------'
-  print "~-==>> #{left_hand2} #{oper} #{right_hand2} = "
-  case oper
-    when "+"
-      answer = left_hand2 + right_hand2
-    when "-"
-      answer = left_hand2 - right_hand2
-    when "*"
-      answer = left_hand2 * right_hand2
-    when "/"
-      answer = left_hand2 / right_hand2
-    else
-      puts "Houston, we have a problem."
+    right_hand = input[oper_loc + 1..input.length].split.join('')
+    right_hand2 = check_number(right_hand)
+
+    puts '-------------------------------'
+    print "~-==>> #{left_hand2} #{oper} #{right_hand2} = "
+    case oper
+      when "+"
+        answer = left_hand2 + right_hand2
+      when "-"
+        answer = left_hand2 - right_hand2
+      when "*"
+        answer = left_hand2 * right_hand2
+      when "/"
+        answer = left_hand2 / right_hand2
+      else
+        puts "Houston, we have a problem."
+    end
+    puts "#{answer} <<==-~"
+    puts '-------------------------------'
+
+  else
+    puts "we don't have an operator location"
+    puts ''
+    oper_loc = nil
+    get_input
   end
-  puts "#{answer} <<==-~"
-  puts '-------------------------------'
-
-  # first_num = get_number
 end
 
 
 while true
-  clear_test = true
-
   puts ''
   puts ''
   puts "-------------------------------".colorize(:light_blue)
-  puts "Welcome to the Ruby calculator!".colorize(:light_blue)
+  puts "|       RUBY CALCULATOR       |".colorize(:light_blue)
   puts "-------------------------------".colorize(:light_blue)
 
   get_input
-
-  # irb(main):002:0> op =~ '+'
-  # => 0
-  # irb(main):003:0> op =~ '123'
-  # => nil
-  # irb(main):004:0> input = '45+234'
-  # => "45+234"
-  # irb(main):005:0> /\d+(\+){1}\d+/ =~ input
-  # => 0
-  # irb(main):006:0> input
-  # => "45+234"
-  # irb(main):007:0> input2 = '23/*123'
-  # => "23/*123"
-  # irb(main):008:0> /\d+(\+){1}\d+/ =~ input2 # checks for equation
-  # => nil
-
-
-
-
-  # first_num = get_number("first")
-  # clear_test = false if first_num == "c"
-  #
-  # oper = get_operator if clear_test
-  # clear_test = false if oper == "c"
-  #
-  # second_num = get_number("second") if clear_test
-  # clear_test = false if second_num == "c"
-  #
-  # if clear_test
-  #   puts '-------------------------------'
-  #   print "~-==>> #{first_num} #{oper} #{second_num} = "
-  #   case oper
-  #     when "+"
-  #       answer = first_num + second_num
-  #     when "-"
-  #       answer = first_num - second_num
-  #     when "*"
-  #       answer = first_num * second_num
-  #     when "/"
-  #       answer = first_num / second_num
-  #     else
-  #       puts "Houston, we have a problem."
-  #   end
-  #   puts "#{answer} <<==-~"
-  #   puts '-------------------------------'
-  #   puts ''
-  #   puts ''
-  # end
 end
